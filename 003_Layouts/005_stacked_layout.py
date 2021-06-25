f5cdeb423234e4aa9122c600b64cf4307551e63b
Joseph Kariuki <joehene@gmail.com>
2021-06-25 11:59:01 +0300

    Created docstring for stacked layout script.

---
+++ 003_Layouts/005_stacked_layout.py
@@ -0,0 +1,18 @@
+"""
+/***************************************************************************
+Name                 : Stacked Layout
+Description          : Stacked layout containing widgets.
+Date                 : 25/June/2021
+copyright            : (C) 2020 by Joseph Kariuki
+email                : joehene@gmail.com
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
